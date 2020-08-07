17601e8d38005d9f9d7f9b94daa24de618ff41c1
Satwik Ram <satwikram29@gmail.com>
2020-08-07 10:37:06 +0530

    updated

--- predictor/views.py
+++ predictor/views.py
@@ -20,19 +20,39 @@ def home(request):
 def predict(request):
     if request.method == "POST":
         tweet = request.POST['tweet']
+
+        # Cleaning the tweet
         tweet = clean_tweet(tweet)
 
+        # Loading Tokenizer
         encoder = tfds.features.text.SubwordTextEncoder.load_from_file("Tokenizer/tokenizer")
         tweet = np.array([tweet])
         print("Tweet is:", tweet)
         print("Tweet of numpy is:",tweet[0])
 
+        # Encoding the tweet
         tweet = encoder.encode(tweet[0])
         print("Encoded text:",tweet)
 
+        # Loading the saved model
+        print("Loading model...")
+        model = tf.saved_model.load("sentiment\saved_model.pb")
 
+        print("Signatures of model:",print(list(model.signatures.keys())))
 
+        predictor = model.signatures["serving_default"]
+        print(predictor.structured_outputs)
 
-        return render(request, 'results.html')
+        result = predictor(tf.constant(tweet))
+
+        result['output_1'][0][0]
+
+        if result['output_1'][0][0] < .5:
+            message = "Oops!, thats a negative twwet :("
+
+        else:
+            message = "Its a positive tweet :)"
+
+        return render(request, 'results.html', {'message': message})
     else:
         return render(request, 'predict.html')
